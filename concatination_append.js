8c621871bd315b9b7b7ecb3256481a3bdf454bff
areeb <areeb.malik@cooperativecomputing.com>
2022-06-21 20:13:50 +0500

    More JS and updated HTML

---
+++ concatination_append.js
@@ -0,0 +1,27 @@
+/**
+ * Concatination
+ * concatination is the process of 2 or more strings
+ * 
+ * 
+ * Append
+ * a method used to insert required content at the end of string or content that already exists 
+ */
+
+let first_name = 'Harry';
+let last_name = 'Potter';
+
+let full_name = first_name + " " + last_name;
+console.log(full_name);
+console.log('');
+
+
+// appending
+// .push allows you to add a value to the end of a list
+let items = ['vegetables', 'beef', 'rice'];
+console.log(items);
+console.log('after pushing the cake');
+console.log('');
+
+items.push('cake');
+console.log(items);
+console.log('');

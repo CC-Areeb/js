edd8bf00d0b24d277ed8b2da470ba7768c7c57d5
areeb <areeb.malik@cooperativecomputing.com>
2022-06-21 17:48:02 +0500

    New JS file and updates in HTML

---
+++ arithmetic.js
@@ -0,0 +1,64 @@
+/**
+ * Basic Arthmetic operations include 
+ * addition
+ * subtraction
+ * multiplication
+ * division
+ */
+
+// addition
+let number1 = 100;
+let number2 = 50;
+let addition = number1 + number2;
+console.log(addition);
+console.log('');
+
+
+// subtraction
+let number3 = 100;
+let number4 = 50;
+let subtraction = number3 - number4;
+console.log(subtraction);
+console.log('');
+
+
+// multiplication
+let number5 = 100;
+let number6 = 50;
+let multiplication = number5 * number6;
+console.log(multiplication);
+console.log('');
+
+
+// division
+let number7 = 100;
+let number8 = 50;
+let division = number7 / number8;
+console.log(division);
+console.log('');
+
+
+// modulus of a number -----> the output tells the remainder after the division
+let number9 = 100;
+let number10 = 50;
+let modulus = number9 % number10;
+console.log(modulus);
+console.log('');
+
+
+// increaments ------> increase a number by 1 
+// number++ will return first and increase after 
+// ++number will increase first and return after
+let number_to_increase = 1;
+number_to_increase++;
+console.log(number_to_increase);
+console.log('');
+
+
+// decreaments ------> decrease a number by 1 
+// number-- will return first and increase after 
+// --number will increase first and return after
+let number_to_decrease = 10;
+number_to_decrease--;
+console.log(number_to_decrease);
+console.log('');
